ea40512e4403dcbb95f4eb65270863aea2318a8a
[Mukesh Kumar Utmani] <[kumarmukesh789123@gmail.com]>
2023-06-21 11:28:32 +0500

    'kumar'

---
+++ operators.js
@@ -0,0 +1,43 @@
+// operators in js
+// arthimatic operators
+let a=10;
+let b=3;
+
+console.log("a+b:",a+b);
+console.log("a-b:",a-b);
+console.log("a/b:",a/b);
+console.log("a*b:",a*b);
+console.log("a power b:",a**b);
+console.log("a%b:",a%b);
+console.log("++a:",++a);  11
+console.log("a++:",a++);  11
+console.log("a--:",a--);  12
+console.log("--a:",--a);  10
+console.log("a",a); 
+console.log("a--",a--);
+console.log("a",a);
+
+// assigment operators
+let d=7;
+console.log(d);
+d+=7;
+console.log(d);
+
+// comparision opertors 
+let var1=5;
+let var2='5';
+console.log(var1!=var2);
+console.log(var1===var2);
+console.log(var1!==var2);
+
+// ternory operators 
+// logical operators
+
+let x=8;
+let y=10;
+console.log(x<y && x==8);
+console.log(x>y || x==8);
+console.log(!true);
+console.log(!false);
+
+
