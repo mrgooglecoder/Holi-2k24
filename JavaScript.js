bc3c1e85a866001783d142d215057ace4440e58f
Mukesh Kumar Utmani <109408900+mukeshkumarutmani@users.noreply.github.com>
2023-06-22 10:12:48 +0500

    Update JavaScript.js
    I have just add some functions code

--- JavaScript.js
+++ JavaScript.js
@@ -41,3 +41,29 @@ console.log(!true);
 console.log(!false);
 
 
+// function in java script 
+function kumar(x,y){
+    return Math.round((x+y)/3);  
+}
+let a =4;
+let b=8;
+let c=9;
+
+console.log("The value is",kumar(a,b));
+console.log("The value is",kumar(a,c));
+console.log("The value is",kumar(b,c));
+
+// arrow functionn
+const sum=(p,q)=>{
+    return p+q;
+}
+console.log(sum(2,5));
+
+function empty(){
+    console.log("hii this is mukesh ullo");
+    return 'studentii';
+}
+
+let v=empty();
+console.log(v)
+
